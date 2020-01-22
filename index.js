55d31449334a6c2257e3f4cf2bf75345dd799244
Daniel Cruz <d.cruz.489@hotmail.com>
2020-01-22 14:36:39 -0700

    finally done

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-// Code away!
+// // Code away!
 const server = require('./server.js');
 const port = 3000;
 server.listen(port, () => console.log(`Listening on port: ${3000}`));
--- users/userRouter.js
+++ users/userRouter.js
@@ -1,47 +1,109 @@
-const express = require('express');
+const express = require("express");
 
 const router = express.Router();
 
-router.post('/', (req, res) => {
+const db = require("./userDb.js");
+const PostDb = require("../posts/postDb.js");
+
+const validateUserId = require("./middleware/validateUserId.js");
+const validateUser = require("./middleware/validateUser");
+const validatePost = require("../posts/middleware/validatePost.js");
+
+router.post('/', validateUser, (req, res) => {
   // do your magic!
+  db.insert(req.body)
+  .then(user => res.status(201).json({ user }))
+  .catch(err =>
+    res.status(500).json({
+      message: "Something went wrong adding user.",
+      error: err,
+      error_message: err.message
+    })
+  );
 });
 
-router.post('/:id/posts', (req, res) => {
+router.post('/:id/posts', validateUserId, validatePost, (req, res) => {
   // do your magic!
+  const newPost = {
+    user_id: req.params.id,
+    text: req.body.text
+  };
+  PostDb.insert(newPost)
+    .then(post => res.status(200).json({ post }))
+    .catch(err =>
+      res.status(500).json({
+        message: "sorry something went wrong adding that post",
+        error: err,
+        error_message: err.message
+      })
+    );
 });
 
 router.get('/', (req, res) => {
   // do your magic!
+  db.get()
+  .then(users => res.status(200).json({ users }))
+  .catch(err =>
+    res.status(500).json({
+      message: "Something went wrong gettings users.",
+      error: err,
+      error_message: err.message
+    })
+  );
 });
 
-router.get('/:id', (req, res) => {
+router.get('/:id', validateUserId, (req, res) => {
   // do your magic!
+  db.getById(req.params.id)
+    .then(user => res.status(200).json({ user }))
+    .catch(err =>
+      res.status(500).json({
+        message: "something went wrong gettings user",
+        error: err,
+        error_message: err.message
+      })
+    );
 });
 
-router.get('/:id/posts', (req, res) => {
+router.get('/:id/posts', validateUserId, (req, res) => {
   // do your magic!
+  db.getUserPosts(req.params.id)
+  .then(posts => res.status(200).json({ posts }))
+  .catch(err =>
+    res.status(500).json({
+      message: "sorry something went wrong in trying to get the users posts",
+      error: err,
+      error_message: err.message
+    })
+  );
 });
 
-router.delete('/:id', (req, res) => {
+router.delete('/:id', validateUserId, (req, res) => {
   // do your magic!
+  db.remove(req.params.id)
+    .then(() =>
+      res.status(200).json({ success_message: "User successfully deleted." })
+    )
+    .catch(err =>
+      res.status(500).json({
+        message: "sorry something went wrong with deleting that user",
+  	    error: err,
+        error_message: err.message
+        })
+      );
 });
 
-router.put('/:id', (req, res) => {
+router.put('/:id', validateUserId, validateUser, (req, res) => {
   // do your magic!
+  db.update(req.params.id, req.body)
+    .then(() => res.status(200).json({ message: "user successfully updated." }))
+    .catch(err =>
+      res.status(500).json({
+        message: "something went wrong updating user",
+        error: err,
+        error_message: err.message
+      })
+    );
 });
-
-//custom middleware
-
-function validateUserId(req, res, next) {
-  // do your magic!
-}
-
-function validateUser(req, res, next) {
-  // do your magic!
-}
-
-function validatePost(req, res, next) {
-  // do your magic!
-}
 
 module.exports = router;
