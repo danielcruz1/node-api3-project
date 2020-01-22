2b4e7e3561a28a4d96ff4ca56ecca70c8860564e
Daniel Cruz <d.cruz.489@hotmail.com>
2020-01-21 20:54:08 -0700

    connected server.js to index.js

--- index.js
+++ index.js
@@ -1,1 +1,4 @@
-// code away!
+// Code away!
+const server = require('./server.js');
+const port = 3000;
+server.listen(port, () => console.log(`Listening on port: ${3000}`));
--- server.js
+++ server.js
@@ -3,10 +3,10 @@ const express = require('express');
 const server = express();
 
 server.get('/', (req, res) => {
-  res.send(`<h2>Let's write some middleware!</h2>`);
+  res.send(`<h2>ALL THE MIDDLEWARE!</h2>`);
 });
 
-//custom middleware
+// custom middleware
 
 function logger(req, res, next) {}
 
